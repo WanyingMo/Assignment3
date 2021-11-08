6be5d0b590afab647b9c0cbfab30a27f04b40018
unknown <wm318@scarletmail.rutgers.edu>
2021-11-08 15:29:25 -0500

    teammates

--- assignment3.R
+++ assignment3.R
@@ -1,3 +1,14 @@
+
+# Team Members:
+
+# Animesh Sharma                - as3592
+# Yaniv Bronshtein              - yb262
+# Wanying Mo                    - wm318
+# Venkata Krishnam Raju Dalta   - vkd20
+# Vipul Gharde                  - vig4
+# Aditya Maheshwari             - am2971
+# Toshitt Ahuja                 - ta498
+# Fan Shen                      - fs470
 moment_estimator <- function(data, distribution) {
   if (distribution == "Binomial") {
     ip = data$data
